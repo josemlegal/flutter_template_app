138327de9a5fc6d001c1004b0388abc012652382
josemlegal <josemlegal97@gmail.com>
2023-07-12 10:29:14 -0400

    Update views barrells and delete login presentation layer

--- lib/presentation/views.dart
+++ lib/presentation/views.dart
@@ -3,6 +3,5 @@ export 'package:flutter_template_app/presentation/random/views/random_view.dart'
 export 'package:flutter_template_app/presentation/auth/views/landing_view.dart';
 
 export 'package:flutter_template_app/presentation/home/views/home_view.dart';
-export 'package:flutter_template_app/presentation/login/views/login_view.dart';
 
 export 'package:flutter_template_app/presentation/auth/views/onboarding_view.dart';
