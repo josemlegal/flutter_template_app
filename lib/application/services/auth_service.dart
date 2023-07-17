0a3e348af9be3e020dbc5c41b88de180d79e5120
doggbmx <manuelorticosta@gmail.com>
2023-07-17 15:24:36 -0400

    submit changes for review

--- lib/application/services/auth_service.dart
+++ lib/application/services/auth_service.dart
@@ -6,12 +6,12 @@ import 'package:flutter_template_app/user/domain/repositories/user_repository.da
 import 'package:flutter_template_app/auth/presentation/controllers/landing_view_controller.dart';
 
 abstract class AuthService {
-  Future<bool> signInWithEmailAndPassword({
-    required SignIn signInType,
-    required String email,
-    required String password,
-  });
-  Future<bool> signInWithOAuth({required SocialSignIn signInType});
+  // Future<bool> signInWithEmailAndPassword({
+  //   required SignIn signInType,
+  //   required String email,
+  //   required String password,
+  // });
+  // Future<bool> signInWithOAuth({required SocialSignIn signInType});
 }
 
 class AuthServiceImplementation implements AuthService {
@@ -24,39 +24,39 @@ class AuthServiceImplementation implements AuthService {
   })  : _authRepository = authRepository ?? locator<AuthRepository>(),
         _userRepository = userRepository ?? locator<UserRepository>();
 
-  @override
-  Future<bool> signInWithEmailAndPassword(
-      {required SignIn signInType,
-      required String email,
-      required String password}) async {
-    if (signInType == SignIn.Signup) {
-      await _authRepository.signUpWithEmail(email: email, password: password);
+  // @override
+  // Future<bool> signInWithEmailAndPassword(
+  //     {required SignIn signInType,
+  //     required String email,
+  //     required String password}) async {
+  //   if (signInType == SignIn.Signup) {
+  //     await _authRepository.signUpWithEmail(email: email, password: password);
 
-      return false;
-    }
-    await _authRepository.signInWithEmail(email: email, password: password);
-    await _userRepository.getUser(_authRepository.userId!);
-    return true;
-  }
+  //     return false;
+  //   }
+  //   await _authRepository.signInWithEmail(email: email, password: password);
+  //   await _userRepository.getUser(_authRepository.userId!);
+  //   return true;
+  // }
 
-  @override
-  Future<bool> signInWithOAuth({required SocialSignIn signInType}) async {
-    if (signInType == SocialSignIn.GoogleSignIn) {
-      log('Google Sign In');
-      await _authRepository.signInWithGoogle();
-      log('despues del sign in');
-    } else {
-      await _authRepository.signInWithApple();
-    }
+  // @override
+  // Future<bool> signInWithOAuth({required SocialSignIn signInType}) async {
+  //   if (signInType == SocialSignIn.GoogleSignIn) {
+  //     log('Google Sign In');
+  //     await _authRepository.signInWithGoogle();
+  //     log('despues del sign in');
+  //   } else {
+  //     await _authRepository.signInWithApple();
+  //   }
 
-    log('antes de checkear la db');
-    final userExists =
-        await _userRepository.getUser(_authRepository.currentUser!.uid);
-    log('despues de la checkear la db');
-    log(userExists.toJson().toString());
-    if (userExists.name == '') {
-      return false;
-    }
-    return true;
-  }
+  //   log('antes de checkear la db');
+  //   final userExists =
+  //       await _userRepository.getUser(_authRepository.currentUser!.uid);
+  //   log('despues de la checkear la db');
+  //   log(userExists.toJson().toString());
+  //   if (userExists.name == '') {
+  //     return false;
+  //   }
+  //   return true;
+  // }
 }
