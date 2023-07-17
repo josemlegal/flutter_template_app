2e3e7040a817a3b517ca8c8cda5970c18b6d75ba
doggbmx <manuelorticosta@gmail.com>
2023-07-17 15:24:08 -0400

    email&pass signIn use case

---
+++ lib/auth/domain/use_cases/sign_in_with_email_and_pass.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_template_app/auth/domain/repositories/auth_repository.dart';
+import 'package:flutter_template_app/auth/presentation/controllers/landing_view_controller.dart';
+import 'package:flutter_template_app/user/domain/repositories/user_repository.dart';
+
+class SignInWithEmailAndPasswordUseCase {
+  final AuthRepository authRepository;
+  final UserRepository userRepository;
+
+  SignInWithEmailAndPasswordUseCase(
+      {required this.authRepository, required this.userRepository});
+
+  Future<bool> call(
+      {required SignIn signInType,
+      required String email,
+      required String password}) async {
+    if (signInType == SignIn.Signup) {
+      await authRepository.signUpWithEmail(email: email, password: password);
+      return false;
+    }
+    await authRepository.signInWithEmail(email: email, password: password);
+    await userRepository.getUser(authRepository.userId!);
+    return true;
+  }
+}
