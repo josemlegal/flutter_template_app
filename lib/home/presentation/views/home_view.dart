3d48908f20a13de7ca39d88f001b8aa9884d2b00
doggbmx <manuelorticosta@gmail.com>
2023-07-18 10:18:45 -0400

    sharing new structure w/ teammate

--- lib/home/presentation/views/home_view.dart
+++ lib/home/presentation/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template_app/home/presentation/controllers/home_view_controller.dart';
 import 'package:flutter_template_app/user/domain/models/user_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -8,15 +9,22 @@ class HomeView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(currentUser.name),
-        ),
-        body: const Center(
-          child: Text('Home View is working'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.logout),
-        ));
+      appBar: AppBar(
+        title: Text(currentUser.name),
+      ),
+      body: Column(
+        children: [
+          const Center(
+            child: Text('Home View is working'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ref.read(homeViewProvider).onLogOut();
+            },
+            child: const Text('LOGOUT'),
+          )
+        ],
+      ),
+    );
   }
 }
