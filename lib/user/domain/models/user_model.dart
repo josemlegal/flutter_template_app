c8a7e2381857b7670f4d650077cf7a32e840076c
doggbmx <manuelorticosta@gmail.com>
2023-07-18 10:58:18 -0400

    adds isVerified property to User

--- lib/user/domain/models/user_model.dart
+++ lib/user/domain/models/user_model.dart
@@ -2,12 +2,14 @@ class User {
   final int? id;
   final String name;
   final String email;
+  final bool? isVerified;
   final String? firebaseId;
 
   User({
     this.id,
     required this.name,
     required this.email,
+    this.isVerified,
     this.firebaseId,
   });
 
@@ -15,6 +17,7 @@ class User {
         id: json["id"],
         name: json["name"],
         email: json["email"],
+        isVerified: json["is_verified"],
         firebaseId: json["firebaseId"],
       );
 
@@ -22,6 +25,7 @@ class User {
         "id": id,
         "name": name,
         "email": email,
+        "is_verified": isVerified,
         "firebaseId": firebaseId,
       };
 
@@ -29,12 +33,14 @@ class User {
     int? id,
     String? name,
     String? email,
+    bool? isVerified,
     String? firebaseId,
   }) {
     return User(
       id: id ?? this.id,
       name: name ?? this.name,
       email: email ?? this.email,
+      isVerified: isVerified ?? this.isVerified,
       firebaseId: firebaseId ?? this.firebaseId,
     );
   }
