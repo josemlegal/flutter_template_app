24f3129e57b6d96994b85a99d502aa08b3393ac3
josemlegal <josemlegal97@gmail.com>
2023-07-25 11:05:48 -0400

    Update the profile view/controller

--- lib/profile/presentation/views/profile_view.dart
+++ lib/profile/presentation/views/profile_view.dart
@@ -1,16 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template_app/profile/presentation/controllers/profile_view_controller.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+// THIS IMPORT IS BEING USED, PLEASE DO NOT REMOVE
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class ProfileView extends StatelessWidget {
-  const ProfileView({super.key});
+class ProfileView extends StatefulHookConsumerWidget {
+  const ProfileView({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<ProfileView> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      if (mounted && ref.read(profileViewProvider).currentUser == null) {
+        await ref.read(profileViewProvider.notifier).getCurrentUser();
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final profileViewController = ref.watch(profileViewProvider);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile View'),
+        title: profileViewController.currentUser == null
+            ? const CircularProgressIndicator()
+            : Text(
+                profileViewController.currentUser!.name,
+                style: const TextStyle(
+                  color: Colors.red,
+                ),
+              ),
       ),
-      body: const Center(
-        child: Text('Profile View'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Profile View',
+            ),
+            profileViewController.isLoading
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: () {
+                      profileViewController.checkEmailVerification();
+                    },
+                    child: const Text('Verificar email'),
+                  ),
+            profileViewController.isResendingEmailLink
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: () {
+                      profileViewController.sendEmailLink();
+                    },
+                    child: const Text('Reenviar email'),
+                  ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(profileViewProvider.notifier).onLogOut();
+              },
+              child: const Text('LOGOUT'),
+            ),
+          ],
+        ),
       ),
     );
   }
